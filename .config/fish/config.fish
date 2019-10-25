bb778b40e84ebd93c5232f0dd6ea8d7730a03382
awray_mac <andrewwray2718@gmail.com>
2019-10-25 10:35:52 -0700

    Add Fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
-
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 eval /usr/local/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
+
+alias vim "nvim"
+alias fishconfig "vim ~/.config/fish/config.fish"
