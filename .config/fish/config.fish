877aba2a279bd4dbfdbb81317512202fd8dbd918
Andrew Wray <andrewwray2718@gmail.com>
2020-02-08 15:22:46 -0800

    Remove unneeded configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,13 +6,13 @@
 
 
 # server alias for vim
-alias vim "vim --servername vim"
+alias vim "nvim"
 
 alias vimconfig "vim ~/.vimrc"
 alias fishconfig "vim ~/.config/fish/config.fish"
-alias spotconfig "vim ~/.config/spotifyd/spotifyd.conf"
-alias visconfig "vim ~/.config/vis/config"
-alias i3statusconfig "vim ~/.config/i3status/config"
+#alias spotconfig "vim ~/.config/spotifyd/spotifyd.conf"
+#alias visconfig "vim ~/.config/vis/config"
+#alias i3statusconfig "vim ~/.config/i3status/config"
 alias texconfig "vim ~/.vim/ftplugin/tex.vim"
 
 alias la 'ls -la | grep "^d"; and ls -la | grep "^-"; and ls -la | grep "^l"'
@@ -38,7 +38,7 @@ alias yc "yadm commit -v"
 alias yst "yadm status"
 
 # Conda settings for fish
-eval /home/andrew/miniconda/bin/conda "shell.fish" "hook" $argv | source
+#eval /home/andrew/miniconda/bin/conda "shell.fish" "hook" $argv | source
 
 # fzf
 set FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
